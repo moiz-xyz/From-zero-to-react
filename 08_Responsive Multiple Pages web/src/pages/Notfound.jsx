ad37e278000cfc975c2f9c4acbcd577f2891d57f
moiz-xyz <a.moizniaxi123@gmail.com>
2025-06-03 14:00:54 +0500

    added not fond route

---
+++ 08_Responsive Multiple Pages web/src/pages/Notfound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NotFound from "../../public/image.jpeg"
+
+const Notfound = () => {
+  return (
+    <div>
+      <img src={NotFound} alt="" />
+    </div>
+  )
+}
+
+export default Notfound
--- 08_Responsive Multiple Pages web/src/routes/Router.jsx
+++ 08_Responsive Multiple Pages web/src/routes/Router.jsx
@@ -5,16 +5,19 @@ import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Country from '../pages/Country';
 import Layout from './Layout'; // ✅ Import the layout
+import Notfound from '../pages/Notfound';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} /> 
-        <Route path='about' element={<About />} />
         <Route path='contact' element={<Contact />} />
+        <Route path='home' element={<Home />} />
+        <Route path='about' element={<About />} />
         <Route path='country' element={<Country />} />
-      </Route>
+        </Route>
+        <Route path='*' element = { <Notfound />} /> 
     </Routes>
   );
 };
