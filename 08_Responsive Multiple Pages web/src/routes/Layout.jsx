6de8756c94bcab7e0b77e4459f087ccc4e6cf922
moiz-xyz <a.moizniaxi123@gmail.com>
2025-06-03 13:49:56 +0500

    added dyanamic routing footer and navbar same on evry page

---
+++ 08_Responsive Multiple Pages web/src/routes/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../componets/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../componets/Footer'
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar/>
+        <main>
+      <Outlet/>
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Layout
--- 08_Responsive Multiple Pages web/src/routes/Router.jsx
+++ 08_Responsive Multiple Pages web/src/routes/Router.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { Routes , Route } from 'react-router-dom'
-import Home from '../pages/Home'
-import About from '../pages/About'
-import Contact from '../pages/Contact'
-import Country from '../pages/Country'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Country from '../pages/Country';
+import Layout from './Layout'; // ✅ Import the layout
 
 const Router = () => {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element= {<Home/>}></Route>
-        <Route path='/about' element= {<About/>}></Route>
-        <Route path='/contact' element= {<Contact/>}></Route>
-        <Route path='/country' element= {<Country/>}></Route>
-      </Routes>
-    </div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} /> 
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='country' element={<Country />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default Router
+export default Router;
