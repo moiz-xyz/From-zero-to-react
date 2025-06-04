f357f35b742bb7fea27c7fa10021716bb1bd2332
moiz-xyz <a.moizniaxi123@gmail.com>
2025-06-04 15:34:47 +0500

    add here sections

--- 08_Responsive Multiple Pages web/src/pages/Home.jsx
+++ 08_Responsive Multiple Pages web/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import "../App.css"
 
 const Home = () => {
   return (
     <div>
-      Home
+     <div className='parent'>
+      <div className='first'>
+      <h2>Explore the World One Country at a time </h2>
+      <p>Discover he history culture and beauty of every nation.Sort search and filter through countries to find the details you need</p>
+      <button className='btn1'>Start Exploring</button>
+      </div>
+      <div className='second'>
+hello
+      </div>
+     </div>
     </div>
   )
 }
